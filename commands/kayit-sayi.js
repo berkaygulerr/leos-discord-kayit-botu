301a36ddb9a1b4d160aef531e2b8eb2e7b118a63
berkay guler <berkay4058@gmail.com>
2021-07-28 18:00:18 +0300

    fix: kayit-sayi command
    Signed-off-by: berkay guler <berkay4058@gmail.com>

--- commands/kayit-sayi.js
+++ commands/kayit-sayi.js
@@ -1,5 +1,7 @@
 const Guild = require("../database/models/guildSchema");
 const Discord = require("discord.js");
+const disbut = require("discord-buttons");
+const disbutpages = require("discord-embeds-pages-buttons");
 
 module.exports = {
   name: "kayit-sayi",
@@ -28,33 +30,64 @@ module.exports = {
     var registrationAmounts = guildProfile.registrationAmounts;
     registrationAmounts.sort((a, b) => b.count - a.count);
 
-    var countText = "";
     var count = 1;
 
     if (!args.length) {
-      registrationAmounts.map((registrant) => {
-        countText +=
-          `**${count}.** <@${registrant.id}> kayıt sayısı: **${registrant.count}**\nID: ` +
-          "`" +
-          registrant.id +
-          "`" +
-          "\n\n";
+      var registrantBySlice = [];
+      var pages = [];
 
-        count++;
-      });
+      var i,
+        j,
+        temporary,
+        chunk = 10;
+      for (i = 0, j = registrationAmounts.length; i < j; i += chunk) {
+        temporary = registrationAmounts.slice(i, i + chunk);
+        await registrantBySlice.push(temporary);
+      }
 
-      let embed = new Discord.MessageEmbed()
-        .setAuthor(client.user.username, client.user.displayAvatarURL())
-        .setTitle(`Kayıt Yetkililerinin Kayıt Sayıları`)
-        .setThumbnail(message.guild.iconURL())
-        .addField(
-          "Kayıt Edebilen Yetkili Rolleri:",
-          registerRoles + "\nID: " + "`" + `${regIds}` + "`"
-        )
-        .addField("SIRALAMA:", countText)
-        .setColor("BLUE");
+      for (let i = 0; i < registrantBySlice.length; i++) {
+        var countText = "";
+        registrantBySlice[i].map((registrant) => {
+          countText +=
+            `**${count}.** <@${registrant.id}> kayıt sayısı: **${registrant.count}**\nID: ` +
+            "`" +
+            registrant.id +
+            "`" +
+            "\n\n";
 
-      message.channel.send(embed);
+          count++;
+        });
+
+        var embed = new Discord.MessageEmbed()
+          .setAuthor(client.user.username, client.user.displayAvatarURL())
+          .setTitle(`Kayıt Yetkililerinin Kayıt Sayıları`)
+          .setThumbnail(message.guild.iconURL())
+          .addField(
+            "Kayıt Edebilen Yetkili Rolleri:",
+            registerRoles + "\nID: " + "`" + `${regIds}` + "`"
+          )
+          .addField("SIRALAMA:", countText)
+          .setColor("BLUE")
+          .setFooter(`Sayfa ${i + 1}/${registrantBySlice.length}`);
+
+        pages.push(embed);
+      }
+
+      if (registrantBySlice.length > 1) {
+        disbutpages.pages(
+          client,
+          message,
+          pages,
+          100000,
+          disbut,
+          "green",
+          "⏩",
+          "⏪",
+          "❌"
+        );
+      } else {
+        message.channel.send(pages[0]);
+      }
     } else {
       var mentionMember =
         message.mentions.members.first() ||
